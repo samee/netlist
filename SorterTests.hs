66b8f32fdb815b3fa16a22c397a8f375aecb13c1
Samee Zahur <samee@virginia.edu>
2012-09-02 23:36:29 -0400

    Added Stack tests

--- SorterTests.hs
+++ SorterTests.hs
@@ -1,7 +1,9 @@
 import Test.Gcil
 import Test.Sorter
 import Test.StableMarriage
+import Test.Stack
 
 main = do Test.Sorter.runTests
           Test.Gcil.runTests
-          Test.StableMarriage.runTests
+          Test.Stack.runTests
+          -- :( Test.StableMarriage.runTests
